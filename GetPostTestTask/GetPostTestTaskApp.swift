7f0f07b4ab68524c03cd5b31bc89b4bf5c5d6f98
denysooz <den.darevskiy@mail.ru>
2024-10-03 16:11:38 +0300

    Initial Commit

---
+++ GetPostTestTask/GetPostTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetPostTestTaskApp.swift
+//  GetPostTestTask
+//
+//  Created by Denis Dareuskiy on 3.10.24.
+//
+
+import SwiftUI
+
+@main
+struct GetPostTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
